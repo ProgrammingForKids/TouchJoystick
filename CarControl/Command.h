ba291ce3691a0e94a46f7558efb643bcff187dfa
nopete <peter.novikov@gmail.com>
2016-10-09 23:44:00 +0300

    New formats introduced

--- CarControl/Command.h
+++ CarControl/Command.h
@@ -3,14 +3,53 @@
 
 struct Command
 {
-  const unsigned short _sector : 4;// 0 - means forward, counting clockwise
-  const unsigned short _speed : 4;//
+  enum eProtocol { 
+    protoPolar = B00,
+    protoTank = B01
+  };
+  
+  const eProtocol _protocol : 2;
 
-  Command(byte b)
-  : _sector(b&0x0f)
-  , _speed((b>>4)&0x0f)
+protected:
+  Command(eProtocol prot)
+  : _protocol(prot)
   {
   }
 };
 
+struct CommandPolar
+{
+  const uint8_t _sector : 3;// 0 - means forward, counting clockwise
+  const uint8_t _speed : 3;//
+
+  CommandPolar(byte b)
+  : Command(eProtocol::protoPolar)
+  , _sector((b&B00111000)>>3)
+  , _speed(b&B00000111)
+  {
+  }
+};
+
+struct CommandTank
+{
+  struct Track
+  {
+    const bool _reverse : 1;
+    const uint8_t _speed : 2;
+    Track(byte b)
+    : _reverse(b&B100)
+    , _speed(b&B011)
+    {}
+  };
+  
+  const Track _left;
+  const Track _right;
+
+  CommandTank(byte b)
+  : Command(eProtocol::protoTank)
+  , _left((b&B00111000)>>3)
+  , _right(b&B00000111)
+  {
+  }
+};
 #endif // _ProgrammingForKids_TouchJoystick__Command_h__
